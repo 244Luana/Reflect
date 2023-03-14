92ef514d1bdf9c611ffce17301ec0586f7efdf82
Luana Amaral <244luana@gmail.com>
2023-03-14 20:56:44 -0300

    basic configs

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from './Components/Menu/Header';
+
+function Home() {
+  return (
+    <div className="Home">
+      {/* <Header /> */}
+    </div>
+  );
+}
+
+export default Home;
