cc097f4cb95a13a180e52bf3c4934e2b95eee049
Luana Amaral <244luana@gmail.com>
2023-03-14 20:36:36 -0300

    basic configs

---
+++ src/Components/Menu/Header.jsx
@@ -0,0 +1,26 @@
+// import React from "react";
+
+// function Header() {
+//   // const Title = (
+//   //   <h1> 
+//   //     This Is Reflect
+//   //   </h1>
+//   // );
+
+//   // const Line = (
+
+//   // );
+  
+  
+//   // return (
+//     // <div className="Home">
+//     //   {/* <header>
+//     //     <p> 
+//     //         When Will My Reflection Show Who I Am Inside?
+//     //     </p> 
+//     //   </header> */}
+//     // </div>
+//   // );
+// }
+
+// export default Header;
