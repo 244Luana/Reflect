37a28ab94ecd1cfaf0301b300e6cf5e58ca69ef6
Luana Amaral <244luana@gmail.com>
2023-04-21 22:20:12 -0300

    Config header

--- src/Components/Menu/Header.jsx
+++ src/Components/Menu/Header.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { Button } from '../Utils/Button'
+import { Title, MenuSelection } from '../Utils/MenuTitle';
+import { Button } from '../Utils/Button';
 
 function Header() {
   
   return (
     <div className="Home">
       <header>
-        <title align-itens='center'> 
-            This Is Reflect
-        </title>
+        <Title> 
+            THIS IS REFLECT
+        </Title>
+        <MenuSelection>
+          <Button> HOME </Button>
+          <Button> ABOUT ME </Button>
+          <Button> NEWS </Button>
+          <Button> MY PROJECTS </Button>
+          <Button> REFLECTIONS </Button>
+        </MenuSelection>
         <p> 
             When Will My Reflection Show Who I Am Inside?
         </p> 
-        <Button/>
       </header>
     </div>
   );
